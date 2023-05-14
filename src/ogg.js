15412a8d80a81c2012663359492c1b2c0977aadc
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-05-14 17:28:53 +0200

    complete method create in ogg.js

--- src/ogg.js
+++ src/ogg.js
@@ -1,10 +1,35 @@
+import axios from "axios";
+import { createWriteStream } from "fs"
+import {dirname,resolve} from "path";
+import {fileURLToPath} from "url"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+
 class OggConverter {
   constructor() {
   }
   toMpe() {}
 
-  create() {}
+  async create (url, filename) {
+    try {
+      const oggPath = resolve(__dirname, '../voices', `${filename}.ogg`)
+      const response = await axios({
+        method: "GET",
+        url,
+        responseType: "stream"
+      })
+      return new Promise(resolve => {
+        const stream = createWriteStream(oggPath)
+        response.data.pipe(stream)
+        stream.on('finish', () => resolve(oggPath))
+      })
 
+    } catch (error) {
+      console.log('Error while creating ogg file', error.message);
+    }
+
+  }
 }
 
 export const ogg = new OggConverter();
